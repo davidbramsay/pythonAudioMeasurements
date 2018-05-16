8b65974355a7e9d7a048ef63f91cee427711810e
David Ramsay <dramsay@mit.edu>
2018-05-16 17:25:27 -0400

    saving status, asking for metadata

--- polarPlot.py
+++ polarPlot.py
@@ -6,21 +6,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 from scipy import interpolate
+import cPickle as pickle
 
 
 class polarPlot(object):
 
     # as of right now, you pass in a pyStepper motor instance
-    def __init__(self, usingMotor=None, makeMotor=False, board="/dev/cu.usbmodem1441", pins=[2,3,4,5]):
+    def __init__(self, usingMotor=None, makeMotor=False, board="/dev/cu.usbmodem1441", pins=[2,3,4,5], testSignalSamples=8192, testSignalRepetitions=30, Fs=44100, channels=1):
 
-        self.audioMeas= audioMeasure()
+        self.audioMeas= audioMeasure(Fs=Fs)
         self.allFrequencies = None
         self.response = None
         self.board = None
         self.motor = None
+        self.channels = channels
+
+        # create test signal
+        self.audioMeas.pinkNoiseLoop(samples=testSignalSamples, repetitions=testSignalRepetitions, Fs=Fs)
 
         # initiate and set up motor if needed
-        if usingMotor: 
+        if usingMotor:
             self.motor = usingMotor
 
         if makeMotor:
@@ -71,58 +76,55 @@ class polarPlot(object):
         (list) containing frequency-domain data for the measurement
         """
 
-        # create noise loop
-        self.audioMeas.pinkNoiseLoop(repetitions=30)
-
         # run tests
-        self.audioMeas.testAllChannels()
+        self.audioMeas.testAllChannels(channels=self.channels)
         self.audioMeas.calcTF()
 
         if test:
             self.audioMeas.plotFreqResp() #plot freq response of each channel
             self.audioMeas.plotImpulseResp() #plot IR of each channel
-        
-        # extract tf values note type is 
+
+        # extract tf values note type is
         # audioSample
-        audioSamp = self.audioMeas.tf[0] 
+        audioSamp = self.audioMeas.tf[0]
 
         audioSamp.toDb()
 
 
-        if self.allFrequencies is None: 
-            # contains a list of integers for all 
+        if self.allFrequencies is None:
+            # contains a list of integers for all
             # the frequencies collected
             self.allFrequencies = [int(f) for f in audioSamp.f()]
 
-        
 
-        # return just a list containing the values of the 
+
+        # return just a list containing the values of the
         # transfer function in freq units
-        return audioSamp.data
+        return audioSamp
 
     def collectData(self, numMeasurements=4, measurementFrequencies=None, degreeSeparation=None):
         """
         Args:
         numMeasurements (int): number of measurements to be taken evenly spaced around the circle
-                               if motor in use, then with value 2, measurements will be taken at 
+                               if motor in use, then with value 2, measurements will be taken at
                                0 and 180 degrees
-        measurementFrequencies (list): frequencies to create a plot for. 
+        measurementFrequencies (list): frequencies to create a plot for.
                                         ## current;y: throws out any frequencies not measured
-                                        ## future: calculate the closest frequency and 
+                                        ## future: calculate the closest frequency and
         """
 
-        
+
         """ think this is unnecessary (handled in constructor) """
         if self.allFrequencies is None:
             # all frequencies will be collected in measure
-            # this is because if this has not been set yet, 
+            # this is because if this has not been set yet,
             # then a transfer function has not yet been calculated
             self.measure()
 
 
-        
-        if measurementFrequencies is not None: 
-            
+
+        if measurementFrequencies is not None:
+
             # ensures all inputted frequencies are integers
             # held as set
             measurementFrequencies = {int(f) for f in measurementFrequencies}
@@ -131,7 +133,7 @@ class polarPlot(object):
             toAdd = set()
 
             # make sure all inputted frequencies will be measured
-            # save ones not measured to be removed later 
+            # save ones not measured to be removed later
             for f in measurementFrequencies:
                 if f not in self.allFrequencies:
                     toRemove.add(f)
@@ -148,10 +150,10 @@ class polarPlot(object):
             print("auto assigning frequencies...")
             measurementFrequencies = {self.allFrequencies[i] for i in range(0, len(self.allFrequencies), len(self.allFrequencies)//4)}
             print(measurementFrequencies)
-  
 
-        
-        # contains the indexes in the input data that the given frequency's 
+
+
+        # contains the indexes in the input data that the given frequency's
         # data will appear
         indexes = {f : self.allFrequencies.index(f) for f in measurementFrequencies}
 
@@ -165,32 +167,84 @@ class polarPlot(object):
             degreeSeparation = 360//numMeasurements
 
 
+        #collect some relevant information from the user
+        pickleName = input("please enter a filename (without extension):")
+        pickleFile = './data/' + str(pickleName) + '.pkl'
+
+        micManufacturer = input("please enter manufacturer/maker:")
+        micModel = input("please enter microphone model:")
+
+        print 'axis of measurements:'
+        print '\tfor MEMs mics, the short dimension closest to the port is facing 0 degrees.  For Electrets, 0 degrees will be along the axis that splits the pins' \
+              ', towards the "top" if neg/gnd pin on the right looking top down (through the mic).  Y runs along the 0-180 degree axis, X runs along the 90-270 ' \
+              'degree axis, (as if looking at a polar with 0 degrees on top, with axes labeled appropriately. Z runs through the axis of symmetry of the ' \
+              'microphone (positive towards the ceiling).\n\n\'x-y\' in plane of floor with mic/array facing upwards (primary direction you expect to be used).  Y=0 degrees.
+              '\n\'x-z\' is with positive/0-Y direction towards the ceiling, in the X-Z plane, with positive-Z facing at 0 degrees.'
+
+        measureAxis = input("please enter axis of measurement (\'x-y\', \'x-z\' (default: x-y):')
+        if not measureAxis: measureAxis = "x-y"
+
+        offset = input("please enter offset from 0,0 in rotation dimension, POS-Y = 0 degrees toward speaker (default: (0,0)):")
+        if not offset: offset = (0, 0)
+
+        location = input("please enter the location of this measurement (default: MIT Anechoic Chamber, Blg 41):")
+        if not location: location = "MIT Anechoic Chamber, Blg 41"
+
+        speaker =input("please enter the speaker used for this measurement (default: Equator Audio D5):")
+        if not speaker: speaker = "Equator Audio D5"
+
+        extraInfo = input("please enter any additional description of this measurement:")
+
+        input('MAKE SURE THE DEVICE IS: (1) CENTERED IN FLOOR-CEILING DIM WITH CENTER OF SPEAKER, (2) ALIGNED ALONG 0 AXIS, (3) TURNTABLE IS FACING SHORT DIM, (4) DISTANCE' \
+              ' IS APPROX 2.5M, (5) SPEAKER IS LINEAR BUT LOUD.  OK?')
+
+        timestamp = time.time()
+
+        metadata = {
+            'filename': pickleFile,
+            'manufacturer': micManufacturer,
+            'model': micModel,
+            'measureAxis': measureAxis,
+            'offset': offset,
+            'location': location,
+            'speaker': speaker,
+            'description': extraInfo,
+            'timestamp': timestamp,
+            'degreeRes': degreeSeparation
+        }
+
+        polarData = {'angles': [], 'measurements': []}
+
         # loops through a whole turn
         # evenly dividing into numMeasurements steps
         # rounding down
         """potentially store the angles measured in a serparate list to avoid
             repetitious data/excessive memory usage"""
         for degrees in range(0,360, degreeSeparation):
-            
+
 
             # set the motor to the correct position
-            if self.motor: 
+            if self.motor:
                 self.motor.set_position(degrees)
             else:
                 # count down and allow for the operator
                 # to manually move the motor
                 self.countDown(3)
 
-            data = self.measure() 
+            audiomeas = self.measure()
+
+            polarData['angles'].append(degrees)
+            polarData['measurements'].append(audiomeas)
+            pickle.dump((polarData, metadata), open(metadata['filename'], 'wb'))
 
             for freq in measurementFrequencies:
-                
-                # index in data this frequency's amp should be located at
-                index = indexes[freq] 
 
-            
+                # index in data this frequency's amp should be located at
+                index = indexes[freq]
+
+
                 # add (angle in degrees, real part of amplitude) to this frequency's data list
-                freqData[freq].append((degrees, data[index].real))
+                freqData[freq].append((degrees, audiomeas.data[index].real))
 
 
         # coordinates now contains the appropriate data,, now need to plot them
@@ -205,17 +259,17 @@ class polarPlot(object):
         measured_data (dict): contains alread-measured data in the format
                             {frequecy1 : [(angle1 in degrees, amplitude1), ...], ....}
         """
-        
+
         # suplot on which all data will be places
         ax = plt.subplot(1, 1, 1, projection = "polar")
-        
+
         # collects strings of the frequencies plotted
         # to create a legend at the end
         legend = []
-        
-        #just 3rd one for now plot the first one for 
+
+        #just 3rd one for now plot the first one for
         for freq in measured_data:
-            
+
 
             # add this frequency to the key to be shown
             # on the output grid
@@ -227,7 +281,7 @@ class polarPlot(object):
 
             # unpack the data
             # comes out in tuple
-            theta, r = zip(*data) 
+            theta, r = zip(*data)
 
             # add to ends to make plot loop
             # all the way around
@@ -237,16 +291,16 @@ class polarPlot(object):
 
             theta = [t*np.pi/180 for t in theta] # convert to rad
 
-            
+
             # linear interpolation between points
             """ perhaps a different interpolation would be better
                 linear interpolation in polar leads to naturally out-swooping arcs"""
             f = interpolate.interp1d(theta, r)
-            
+
             # create thetas
             theta_plot = np.arange(0,2*np.pi, 0.05)
-            
-            # using function tp interpolate r-points for 
+
+            # using function tp interpolate r-points for
             # smoothness of output graphic
             r_plot = f(theta_plot)
 
@@ -254,12 +308,12 @@ class polarPlot(object):
             ax.plot(theta_plot, r_plot)
 
         # add graphic title
-        ax.set_title("MICROPHONE RESPONCE AT VARIOUS FREQUENCIES")
+        ax.set_title("MICROPHONE RESPONSE AT VARIOUS FREQUENCIES")
         ax.grid(True) # turn on grid lines
         ax.set_rticks(np.arange(0,50, 10)) # add tick marks
         ax.legend(legend, loc = "upper left") # create key
         plt.show()
-    
+
     def countDown(self, sec):
         """
         Counts down from the given number of seconds, allowing hand-adjustment of  the microphone
@@ -273,5 +327,5 @@ class polarPlot(object):
             time.sleep(1)
             sec -= 1
         print("\n\n")
-            
+
 
