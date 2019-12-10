full array simulation. bug fix on phase calculation

mic_testing.py
@@ -106,21 +106,23 @@ def simulate_polar_1mic():
     plt.show()
 
 
-def simulate_polar_2mic():
+def simulate_polar_array():
 
     filename = "/home/terrasa/UROP/polar-measurement/data/19_Jan15/spv1840.pkl" 
     pd = polarData.fromPkl(filename)
 
 
-    f_options = [440]
+    f_options = [440,1000]
 
     # pd.plotAngle(90)
 
     # position in mm
     mic_1 = Microphone(pd, [0,0])
     mic_2 = Microphone(pd, [390, 0])
+    mic_3 = Microphone(pd, [100, 230])
 
-    mic_array = MicrophoneArray([mic_1, mic_2])
+    mic_array = MicrophoneArray([mic_1, mic_2, mic_3])
+    mic_array.visualize()
 
     fs = 44.1e3
     n = np.arange(10001)
@@ -131,7 +133,7 @@ def simulate_polar_2mic():
         sin_wave = audioSample(sin_wave, type="t", Fs=fs)
 
 
-        thetas = np.array(list(range(0, 361, 1)))
+        thetas = np.array(list(range(0, 361, 5)))
         mags = []
 
         for theta in thetas:
@@ -139,8 +141,8 @@ def simulate_polar_2mic():
             result = mic_array.apply(sin_wave, theta)
             result.toTime()
 
-            # result.plot(True)
-            mags.append(max(result))
+            # result.plot()
+            mags.append(max(result[len(result)//3:len(result)*2//3]))
 
         plt.polar(thetas*np.pi/180, mags)
 
@@ -153,4 +155,4 @@ def simulate_polar_2mic():
 if __name__ == "__main__":
     # test_mic_apply()
     # test_xy()
-    simulate_polar_2mic()
+    simulate_polar_array()

microphone.py
@@ -73,9 +73,23 @@ class Microphone:
         # time-domain shift
         delta_t =  self.normal_origin_dist(theta)/self.c
 
-        phase_shift = np.exp(-1j*np.pi*freqs*delta_t)
+        phase_shift = np.exp(-1j*2*np.pi*freqs*delta_t)
 
-        return audioSample(signal*phase_shift, type=signal.type,Fs=signal.fs)
+        # for f, phi in zip(freqs, phase_shift):
+        #     if f > 439:
+        #         print(f, phi)
+        #         break
+
+        # plt.plot(freqs, np.pi*freqs*delta_t)
+        # plt.yticks(np.arange(0, max(np.pi*freqs*delta_t), 2*np.pi))
+        # plt.plot([440,440], [-1,1])
+        # plt.title("%f for %f, %f"%(theta, self.position[0], self.position[0]))
+        # plt.show()
+
+        result = audioSample(signal*phase_shift, type="f",Fs=signal.fs) 
+        signal.toTime()
+
+        return result
 
 
     def apply_microphone(self, signal, theta, f_targ=None):
@@ -88,12 +102,12 @@ class Microphone:
         mic_response = self.polar.getData(theta)
         mic_response.removeDCOffset()
 
-        if f_targ:
-            for i, f in enumerate(mic_response.f()):
-                if f >= f_targ: 
-                    mic_response.toFreq()
+        # if f_targ:
+        #     for i, f in enumerate(mic_response.f()):
+        #         if f >= f_targ: 
+        #             mic_response.toFreq()
                     # print(f, abs(mic_response[i]))
-                    break
+                    # break
 
         # must have signal of same fs as mic response
         assert mic_response.fs == signal.fs, "your input signal must have the same " + \

microphoneArray.py
@@ -19,15 +19,36 @@ class MicrophoneArray:
 
     def apply(self, signal, theta):
 
+        signal.toTime()
         result = np.zeros(len(signal))
-        print(len(signal))
 
         for mic in self.microphones:
             this_result = mic.apply(signal, theta)
             this_result.toTime()
-            # print(len(this_result))
             result += this_result.data[:len(signal)] # accounts for 1-off from even-lengthsed signals
 
         return audioSample(result, type=signal.type, Fs=signal.fs)
+
+    def apply_xy(self, signal, theta):
+
+        signal.toTime()
+        result = np.zeros(len(signal))
+
+        for mic in self.microphones:
+            this_result = mic.apply_xy(signal, theta)
+            this_result.toTime()
+            result += this_result.data[:len(signal)] # accounts for 1-off from even-lengthsed signals
+
+        return audioSample(result, type=signal.type, Fs=signal.fs)
+
+
+    def visualize(self):
+
+        x = [mic.position[0] for mic in self.microphones]
+        y = [mic.position[1] for mic in self.microphones]
+
+        plt.plot(x, y, "b*")
+
+        plt.show()
 
 